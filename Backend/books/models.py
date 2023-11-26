11ecf32ba529d8a7b7239c9d4e008a7d37fededf
Adamsonbor <adamsonbot@mail.ru>
2023-11-26 21:38:37 +0400

    fix books primary key editable (isbn should not be editable)

--- Backend/books/models.py
+++ Backend/books/models.py
@@ -3,9 +3,17 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    isbn = models.CharField(max_length=13, unique=True, primary_key=True)
-    title = models.CharField(max_length=100, blank=False, null=False)
-    author = models.CharField(max_length=100, blank=False, null=False)
+    isbn = models.CharField(max_length=13,
+                            primary_key=True)
+
+    title = models.CharField(max_length=100,
+                             blank=False,
+                             null=False)
+
+    author = models.CharField(max_length=100,
+                              blank=False,
+                              null=False)
+
     year = models.IntegerField(blank=False,
                                null=False,
                                validators=[MinValueValidator(1)])
